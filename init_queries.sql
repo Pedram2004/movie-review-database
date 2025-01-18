32552d1252361362cd5f1c08f43b9b32b1d59a6d
Pedram Alizadeh <83alizadeh13ped@gmail.com>
2025-01-18 20:27:31 +0330

    Other relations in init_queries edited partially

--- init_queries.sql
+++ init_queries.sql
@@ -11,19 +11,27 @@ CREATE TABLE [User] (
 
 -- Movie / Series Relation
 CREATE TABLE Movie_Series (
-    production_id INT IDENTITY(1,1),
+    production_id INT IDENTITY(1,2),
     title VARCHAR(100) NOT NULL,
     genre VARCHAR(20) NOT NULL,
-    revenue INT NOT NULL,
-    release_date DATE NOT NULL,
+    revenue INT,
+    release_date DATE,
+	[type] VARCHAR(6) NOT NULL CHECK ([type] IN ('Movie', 'Series')),
     imdb_rating NUMERIC(3, 1) CHECK (imdb_rating BETWEEN 0 AND 10),
 	PRIMARY KEY (production_id)
+);
+
+-- Season Relation
+CREATE TABLE Series_Season (
+	season_id INT NOT NULL IDENTITY(2,2),
+	season_number INT NOT NULL,
+	PRIMARY KEY (season_id)
 );
 
 -- Rating Relation
 CREATE TABLE Rating (
     rating_id INT NOT NULL IDENTITY(1,1),
-    rating_value DECIMAL(3, 1) NOT NULL CHECK (rating_value BETWEEN 0 AND 10),
+    rating_value Numeric(3, 1) NOT NULL CHECK (rating_value BETWEEN 0 AND 10),
     REVIEW_text VARCHAR(1000),
 	PRIMARY KEY (rating_id)
 );
@@ -70,11 +78,17 @@ CREATE TABLE User_List (
 	PRIMARY KEY ([user_id], list_id),
     FOREIGN KEY ([user_id]) REFERENCES [User]([user_id]),
     FOREIGN KEY (list_id) REFERENCES Lists(list_id),
-
 );
 
--- جدول ارتباط Movie و List
-CREATE TABLE Movie_List (
+CREATE TABLE Season_Of_Series (
+	season_id INT NOT NULL,
+	production_id INT NOT NULL CHECK (production_id IN (SELECT MS.production_id FROM Movie_Series AS MS WHERE MS.[type] = 'Series')),
+	PRIMARY KEY (season_id, production_id),
+	FOREIGN KEY (production_id) REFERENCES Movie_Series(production_id) ON DELETE CASCADE 
+);
+
+-- A Season or Movie / Series added to a List
+CREATE TABLE Added_To_List (
     production_id INT NOT NULL,
     list_id INT NOT NULL,
     date_added DATETIME NOT NULL, -- DEFAULT CURRENT_TIMESTAMP
@@ -83,30 +97,30 @@ CREATE TABLE Movie_List (
     FOREIGN KEY (list_id) REFERENCES Lists(list_id) ON DELETE CASCADE
 );
 
--- جدول ارتباط Movie و Company
-CREATE TABLE Movie_Company (
-    movie_id INT NOT NULL,
+-- which Movie / Series is produced by what company
+CREATE TABLE Produced_By (
+    production_id INT NOT NULL,
     company_id INT NOT NULL,
-    PRIMARY KEY (movie_id, company_id),
-    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id) ON DELETE CASCADE,
+    PRIMARY KEY (production_id, company_id),
+    FOREIGN KEY (production_id) REFERENCES Movie_Series(production_id) ON DELETE CASCADE,
     FOREIGN KEY (company_id) REFERENCES Company(company_id) ON DELETE CASCADE
 );
 
--- جدول ارتباط Movie و Person
-CREATE TABLE Movie_Person (
-    movie_id INT NOT NULL,
+-- whcih Person is participiated in which Movie / Series
+CREATE TABLE Cast_Crew (
+    production_id INT NOT NULL,
     person_id INT NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    PRIMARY KEY (movie_id, person_id),
-    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id) ON DELETE CASCADE,
-    FOREIGN KEY (person_id) REFERENCES Person(person_id) ON DELETE CASCADE
+    [role] VARCHAR(8) NOT NULL	CHECK ([role] IN ('director', 'actor')),
+    PRIMARY KEY (production_id, person_id),
+    FOREIGN KEY (production_id) REFERENCES Movie_Series(production_id) ON DELETE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES People(person_id) ON DELETE CASCADE
 );
 
--- جدول ارتباط Movie و Rating
-CREATE TABLE Movie_Rating (
-    movie_id INT NOT NULL,
+-- The relation to identify each rating is for which movie
+CREATE TABLE Rating_For_Movie (
+    production_id INT NOT NULL,
     rating_id INT NOT NULL,
-    PRIMARY KEY (movie_id, rating_id),
-    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id) ON DELETE CASCADE,
+    PRIMARY KEY (production_id, rating_id),
+    FOREIGN KEY (production_id) REFERENCES Movie_Series(production_id) ON DELETE CASCADE,
     FOREIGN KEY (rating_id) REFERENCES Rating(rating_id) ON DELETE CASCADE
 );
